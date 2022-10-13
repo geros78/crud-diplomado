12928a87411b130777d511eb7181d7e7fb257061
Alejandro Otaiza Orellano <alejo.o.o1000@gmail.com>
2022-10-12 21:48:29 -0500

    Agregad la seccion listar frutas y agregar frutar

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,12 +7,26 @@ const Formulario = () => {
     <div className='container mt-5'>
         <h1 className='text-center'>CRUD BASICO</h1>
         <hr/>
+        <div className='row'>
+            <div className='col-8'>
+                <h4 className='text-center'>Listado de Frutas</h4>
+                <ul className='list-group'>
+
+                </ul>
+            </div>
+        <div className='col-4'>
+        <h4 className='text-center'>
+            Agregar Frutas
+        </h4>
+        
         <form>
           <input className='form-control mb-2' type="text" placeholder='Ingrese Fruta'></input>
           <input className='form-control mb-2' type="text" placeholder='Ingrese Descripcion'></input>
           <button className='btn btn-primary btn-block'>Agregar</button>
         </form>
+        </div>
     </div>
+</div>
   )
 }
 

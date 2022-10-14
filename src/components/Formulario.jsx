c7fe89d42a05f9869ad1e3154618d9c1ec20fbc8
Alejandro Otaiza Orellano <alejo.o.o1000@gmail.com>
2022-10-13 22:21:51 -0500

    eliminar imoort nano

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
 import { collection, onSnapshot, addDoc, doc, deleteDoc} from 'firebase/firestore';
-import { async } from '@firebase/util';
+
 
 
 
